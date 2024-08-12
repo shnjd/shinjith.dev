2a0de4884c3638f45111f445bf8df3b811ddf49e
shinjith-dev <shinjithpallikkara@gmail.com>
2024-08-12 21:46:46 +0530

    feat: complete projects
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- components/base/Tag.tsx
+++ components/base/Tag.tsx
@@ -1,13 +1,19 @@
 import { cn } from "@/lib/utils/cn";
 import { HTMLAttributes, ReactNode } from "react";
 
-
-
-export default function Tag({ children, className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+export default function Tag({
+  children,
+  className,
+  ...rest
+}: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("rounded-full border border-overlay/45 bg-gradient-to-tr from-background/25 to-secondary/25 px-3 py-1 text-sm text-secondary-fg backdrop-blur-lg backdrop-saturate-150 dark:text-subtle", className)}
-      {...rest}>
+      className={cn(
+        "rounded-full border border-overlay/45 bg-gradient-to-tr from-background/25 to-secondary/25 px-3 py-1 text-sm text-secondary-fg backdrop-blur-lg backdrop-saturate-150 dark:text-subtle",
+        className,
+      )}
+      {...rest}
+    >
       {children}
     </div>
   );
--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -14,27 +14,29 @@ type ProjectProps = {
 const Project = ({ project, toRight }: ProjectProps) => {
   return (
     <div
-      className={`flex group items-center ${toRight ? "project-grad-right flex-row-reverse text-start" : "project-grad-left flex-row text-end"}`}
+      className={`group flex items-center ${toRight ? "project-grad-right flex-row-reverse text-start" : "project-grad-left flex-row text-end"}`}
     >
-      <div className="relative aspect-video w-1/2 shrink-0 basis-1/2 overflow-hidden duration-500 rounded-md group-hover:rounded-xl transition-all">
+      <div className="relative aspect-video w-1/2 shrink-0 basis-1/2 overflow-hidden rounded-md transition-all duration-500 group-hover:rounded-xl">
         <Image
           src={project.image}
           alt={project.name}
           fill
-          className="object-cover object-top saturate-50 transition-all duration-500 ease-out group-hover:scale-105 group-hover:saturate-100 origin-top"
+          className="origin-top object-cover object-top saturate-50 transition-all duration-500 ease-out group-hover:scale-105 group-hover:saturate-100"
         />
       </div>
 
       <div className={`space-y-3 ${toRight ? "-mr-20" : "-ml-20"}`}>
         <h4 className="text-2xl font-bold">{project.name}</h4>
-        <div className={`
-h-fit space-y-3 rounded-md border border-overlay/75 shadow-lg group-hover:shadow-sm transition-all duration-500 from-background/80 via-surface/50 to-primary/20 p-5 text-subtle backdrop-blur-lg backdrop-saturate-200 
-${toRight ? "bg-gradient-to-bl" : "bg-gradient-to-br"}`}>
+        <div
+          className={`h-fit space-y-3 rounded-md border border-overlay/75 from-background/80 via-surface/50 to-primary/20 p-5 text-subtle shadow-lg backdrop-blur-lg backdrop-saturate-200 transition-all duration-500 group-hover:shadow-sm ${toRight ? "bg-gradient-to-bl" : "bg-gradient-to-br"}`}
+        >
           {project.description.map((pd, index) => (
             <p key={`${project.name}-desc-${index}`}>{pd}</p>
           ))}
 
-          <div className={`flex gap-4 ${toRight ? "justify-start" : "justify-end"}`}>
+          <div
+            className={`flex gap-4 ${toRight ? "justify-start" : "justify-end"}`}
+          >
             <Link href={project.github_url} secondary underline={false}>
               <IconBrandGithub size={20} />
             </Link>
@@ -45,9 +47,13 @@ ${toRight ? "bg-gradient-to-bl" : "bg-gradient-to-br"}`}>
           </div>
         </div>
 
-        <div className={`flex flex-wrap gap-2 ${toRight ? "justify-start" : "justify-end"}`}>
+        <div
+          className={`flex flex-wrap gap-2 ${toRight ? "justify-start" : "justify-end"}`}
+        >
           {project.technologies.map((pt) => (
-            <Tag key={pt} className="shadow-sm">{pt}</Tag>
+            <Tag key={pt} className="shadow-sm">
+              {pt}
+            </Tag>
           ))}
         </div>
       </div>
@@ -57,12 +63,12 @@ ${toRight ? "bg-gradient-to-bl" : "bg-gradient-to-br"}`}>
 
 type ProjectCardProps = {
   project: TProject;
-}
+};
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <div className="w-[30%] p-5 group rounded-md overflow-hidden border border-overlay/75 backdrop-blur-lg backdrop-saturate-200 bg-gradient-to-br from-background/80 via-surface/50 to-primary/20">
-      <div className={`flex gap-4 justify-end mb-4`}>
+    <div className="group w-[30%] overflow-hidden rounded-md border border-overlay/75 bg-gradient-to-br from-background/80 via-surface/50 to-primary/20 p-5 backdrop-blur-lg backdrop-saturate-200">
+      <div className={`mb-4 flex justify-end gap-4`}>
         <Link href={project.github_url} secondary underline={false}>
           <IconBrandGithub size={20} />
         </Link>
@@ -72,24 +78,24 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
         </Link>
       </div>
 
-      <h4 className="text-2xl font-bold mb-1">{project.name}</h4>
+      <h4 className="mb-1 text-2xl font-bold">{project.name}</h4>
 
-      <div className="space-y-3 text-muted mb-4">
+      <div className="mb-4 space-y-3 text-muted">
         {project.description.map((pd, index) => (
           <p key={`${project.name}-desc-${index}`}>{pd}</p>
         ))}
       </div>
 
-
       <div className={`flex flex-wrap gap-2`}>
         {project.technologies.map((pt) => (
-          <Tag key={pt} className="shadow-sm">{pt}</Tag>
+          <Tag key={pt} className="shadow-sm">
+            {pt}
+          </Tag>
         ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default function Projects() {
   return (
@@ -100,18 +106,23 @@ export default function Projects() {
         </p>
 
         <ul className="my-12 space-y-28">
-          {projects.filter(p => p.featured).map((p, index) => (
-            <Project key={p.id} project={p} toRight={index % 2 === 1} />
-          ))}
+          {projects
+            .filter((p) => p.featured)
+            .map((p, index) => (
+              <Project key={p.id} project={p} toRight={index % 2 === 1} />
+            ))}
         </ul>
 
-        <p className="w-fit border-b mt-24 border-secondary-fg py-0.5 text-xl font-medium text-secondary-fg dark:border-secondary dark:text-secondary">
+        <p className="mt-24 w-fit border-b border-secondary-fg py-0.5 text-xl font-medium text-secondary-fg dark:border-secondary dark:text-secondary">
           Other noteworthy projects
         </p>
 
-
-        <div className="py-8 flex flex-wrap gap-6 justify-between project-cards-grad">
-          {projects.filter(p => !p.featured).map(p => <ProjectCard project={p} key={p.id} />)}
+        <div className="project-cards-grad flex flex-wrap justify-between gap-6 py-8">
+          {projects
+            .filter((p) => !p.featured)
+            .map((p) => (
+              <ProjectCard project={p} key={p.id} />
+            ))}
         </div>
       </div>
     </Section>
